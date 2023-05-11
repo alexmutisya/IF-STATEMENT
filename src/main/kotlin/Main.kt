6f6b5688987ce01e68136d66a78923057fb66ef6
alex mutisya <alexrichamerican@gmail.com>
2023-05-11 14:44:36 +0300

    IF STATEMENT KOTLIN
    IF STATEMENT KOTLIN

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+fun main(){
+
+    val interns=100
+    if(interns==0){println("no interns")}
+    if(interns>60){println("more interns")}
+    else {println("less interns")}
+
+
+
+
+    val score=487
+    if(score in 75..100){println("low score")}
+    if(score in 105..200){println("fair score")}
+    if(score in 205..350){println("good score")}
+    if(score in 355..450){println("very good")}
+    if(score in 450..600){println("excellent")}
+    else {print("enter correct score")}
+
+
+
+
+}
